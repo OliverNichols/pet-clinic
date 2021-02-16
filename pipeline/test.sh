d43f9f83cf38d320a9121f4e3bcfc38021d9e5b8
Michael Bunko <73492032+MBunko@users.noreply.github.com>
2021-02-16 11:53:55 +0000

    Dependencies moved to dependencies.sh

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install chromium-chromedriver -y
 export CHROME_BIN=/usr/bin/chromium-browser
-sudo apt install npm -y
-sudo npm install -g @angular/cli@8
-git clone https://github.com/OliverNichols/spring-petclinic-angular
 cd spring-petclinic-angular
-npm install --save-dev @angular/cli@8
-sudo npm install
-sudo npm install -g n
-sudo n install v12.8.1
 PATH="$PATH"
-
-
 ng build
 npm run test-headless
