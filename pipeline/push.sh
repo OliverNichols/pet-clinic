26b7d724eec270175925888d11fb44c5d73fd2ab
Ollie Nichols <66640496+OliverNichols@users.noreply.github.com>
2021-02-17 19:18:38 +0000

    Change image repo credentials to env vars

--- pipeline/push.sh
+++ pipeline/push.sh
@@ -3,6 +3,10 @@
 # Need to have nexus-vm set up in /etc/hosts
 
 image_repo="nexus-vm:8082" # ip or host name (e.g. docker.io)
+
+image_repo_user="admin"
+image_repo_pass="password"
+
 path_to_defaults="pet-clinic/pipeline/nexus/config"
 
 # Copy required configs and restart docker
@@ -10,9 +14,8 @@ sudo cp $path_to_defaults/docker /etc/default/docker
 sudo cp $path_to_defaults/daemon.json /etc/docker/daemon.json
 sudo systemctl restart docker
 
-# Login to nexus repo
-docker login -u admin -p password $image_repo
-docker login -u admin -p password $image_repo
+# Login to image repo
+docker login -u $image_repo_user -p $image_repo_pass $image_repo
 
 # Push docker image
 docker push $image_repo/spring-petclinic-angular
