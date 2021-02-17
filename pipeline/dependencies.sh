3c1d96b8c7bd5538ccfc99b0cf4e5bd00abe0b8d
OliverNichols <ollienichols22@gmail.com>
2021-02-17 09:23:47 +0000

    Dependencies up-to-date

--- pipeline/dependencies.sh
+++ pipeline/dependencies.sh
@@ -2,22 +2,24 @@
 
 git_repo="https://github.com/pet-clinic-team-3/spring-petclinic-angular.git" # repo url for front-end
 
+# Clone down repo
+git clone $git_repo
+cd spring-petclinic-angular
+
 # Install apt dependencies
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 sudo apt install npm -y
 sudo apt install chromium-chromedriver -y
 sudo apt install unzip wget -y
 
 # Install npm dependencies
+sudo npm install
 sudo npm install -g @angular/cli@8
 npm install --save-dev @angular/cli@8
 
 # Install node.js v12
 sudo npm install -g n
 sudo n install v12.8.1
-
-# Clone down repo
-git clone $git_repo
 
 # Install terraform
 wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
@@ -31,4 +33,5 @@ rm terraform_*_linux_*.zip
 # rm -rf node_modules/ package-lock.json
 
 # Install docker
-docker --version || curl https://get.docker.com | sudo bash && sudo usermod -aG docker $USER && newgrp docker
+docker --version || curl https://get.docker.com | sudo bash 
+sudo usermod -aG docker $USER 
