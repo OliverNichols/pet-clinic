43358db334f1e846e4239d2d9e49663a682118fd
Ollie Nichols <66640496+OliverNichols@users.noreply.github.com>
2021-02-17 16:20:00 +0000

    Update build.sh

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -4,4 +4,4 @@
 image_repo="nexus-vm:8082" # ip or host name (e.g. docker.io)
 
 # Build docker image
-docker build -t $image_repo/spring-petclinic-angular spring-petclinic-angular
+docker build -t $image_repo/spring-petclinic-angular .
