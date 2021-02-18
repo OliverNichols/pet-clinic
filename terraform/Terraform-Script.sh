c5fc4a458d1a5da302549fc8b78863f1cf269473
Yusuf Yasin <73299366+ykyasin@users.noreply.github.com>
2021-02-18 14:37:09 +0000

    Update Terraform-Script.sh

--- terraform/Terraform-Script.sh
+++ terraform/Terraform-Script.sh
@@ -12,6 +12,15 @@ echo "Running terraform init"
 
 terraform init
 
+echo "Running terraform plan"
+
+terraform plan -var serviceprinciple_id=$SERVICE_PRINCIPAL \
+-var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
+-var tenant_id=$TENANT_ID \
+-var subscription_id=$SUBSCRIPTION \
+-var ssh_key="$SSH_KEY" \
+-auto-approve
+
 echo "Running terraform import 1"
 
 terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
@@ -29,15 +38,6 @@ terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var subscription_id=$SUBSCRIPTION \
 -var ssh_key="$SSH_KEY" \
 module.cluster.azurerm_kubernetes_cluster.main /subscriptions/$SUBSCRIPTION/resourceGroups/petclinic-aks/providers/Microsoft.ContainerService/managedClusters/petclinic-aks
-
-echo "Running terraform plan"
-
-terraform plan -var serviceprinciple_id=$SERVICE_PRINCIPAL \
--var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
--var tenant_id=$TENANT_ID \
--var subscription_id=$SUBSCRIPTION \
--var ssh_key="$SSH_KEY" \
--auto-approve
 
 echo "Running terraform apply"
 
