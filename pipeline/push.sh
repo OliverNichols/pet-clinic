8c8391ee445ec0e40adceb153f310ebdeab0502e
OliverNichols <ollienichols22@gmail.com>
2021-02-18 09:04:13 +0000

    Edited path for repo to build from

--- pipeline/push.sh
+++ pipeline/push.sh
@@ -16,7 +16,7 @@ path_to_defaults="pet-clinic/pipeline/nexus/config"
 docker login -u $image_repo_usr -p $image_repo_psw $image_repo
 
 # Build docker image
-docker build -t $image_repo/spring-petclinic-angular spring-petclinic-angular
+docker build -t $image_repo/spring-petclinic-angular .
 
 # Push docker image
 docker push $image_repo/spring-petclinic-angular
