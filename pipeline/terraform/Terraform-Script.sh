648239cc2b969eea15c0c90a6f6b4f78dd8bc0e2
OliverNichols <ollienichols22@gmail.com>
2021-02-18 15:22:19 +0000

    Moved terraform into pipeline

---
+++ pipeline/terraform/Terraform-Script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SERVICE_PRINCIPAL=$1
+SERVICE_PRINCIPAL_SECRET=$2
+TENANT_ID=$3
+SUBSCRIPTION=$4
+
+ssh-keygen -t rsa -b 4096 -N ""  -q -f  ~/.ssh/id_rsa
+SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
+
+terraform init
+
+
+terraform plan -var serviceprinciple_id=$SERVICE_PRINCIPAL \
+-var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
+-var tenant_id=$TENANT_ID \
+-var subscription_id=$SUBSCRIPTION \
+-var ssh_key="$SSH_KEY" \
+-auto-approve
+
+
+terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
+-var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
+-var tenant_id=$TENANT_ID \
+-var subscription_id=$SUBSCRIPTION \
+-var ssh_key="$SSH_KEY" \
+module.cluster.azurerm_resource_group.main /subscriptions/$SUBSCRIPTION/resourceGroups/petclinic-aks
+
+
+terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
+-var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
+-var tenant_id=$TENANT_ID \
+-var subscription_id=$SUBSCRIPTION \
+-var ssh_key="$SSH_KEY" \
+module.cluster.azurerm_kubernetes_cluster.main /subscriptions/$SUBSCRIPTION/resourceGroups/petclinic-aks/providers/Microsoft.ContainerService/managedClusters/petclinic-aks
+
+
+terraform apply -var serviceprinciple_id=$SERVICE_PRINCIPAL \
+-var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
+-var tenant_id=$TENANT_ID \
+-var subscription_id=$SUBSCRIPTION \
+-var ssh_key="$SSH_KEY" \
+-auto-approve
+
+
+az aks get-credentials -n petclinic-aks -g petclinic-aks --overwrite-existing
+kubectl apply -f ../k8s
+
+
+
