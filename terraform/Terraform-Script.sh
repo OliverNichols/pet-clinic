899bddcdff5ef7293353ec3134550ca7d3870e9a
Yusuf Yasin <73299366+ykyasin@users.noreply.github.com>
2021-02-18 14:25:15 +0000

    Update Terraform-Script.sh

--- terraform/Terraform-Script.sh
+++ terraform/Terraform-Script.sh
@@ -8,7 +8,11 @@ SUBSCRIPTION=$4
 ssh-keygen -t rsa -b 4096 -N ""  -q -f  ~/.ssh/id_rsa
 SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
 
+echo "Running terraform init"
+
 terraform init
+
+echo "Running terraform plan"
 
 terraform plan -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
@@ -17,12 +21,16 @@ terraform plan -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var ssh_key="$SSH_KEY" \
 -auto-approve
 
+echo "Running terraform import 1"
+
 terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
 -var tenant_id=$TENANT_ID \
 -var subscription_id=$SUBSCRIPTION \
 -var ssh_key="$SSH_KEY" \
 module.cluster.azurerm_resource_group.main /subscriptions/$SUBSCRIPTION/resourceGroups/petclinic-aks
+
+echo "Running terraform import 2"
 
 terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
@@ -31,12 +39,16 @@ terraform import -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var ssh_key="$SSH_KEY" \
 module.cluster.azurerm_kubernetes_cluster.main /subscriptions/$SUBSCRIPTION/resourceGroups/petclinic-aks/providers/Microsoft.ContainerService/managedClusters/petclinic-aks
 
+echo "Running terraform apply"
+
 terraform apply -var serviceprinciple_id=$SERVICE_PRINCIPAL \
 -var serviceprinciple_key="$SERVICE_PRINCIPAL_SECRET" \
 -var tenant_id=$TENANT_ID \
 -var subscription_id=$SUBSCRIPTION \
 -var ssh_key="$SSH_KEY" \
 -auto-approve
+
+echo "Running aks"
 
 az aks get-credentials -n petclinic-aks -g petclinic-aks --overwrite-existing
 
